710a489b15ae08bc752f230cfda1319f5416ee6c
cloudopsdojo <cloudopsdojo@gmail.com>
2025-04-11 16:09:43 +0530

    first commit

--- s3_sns_ntfy.tf
+++ s3_sns_ntfy.tf
@@ -1,42 +1,42 @@
-# data "aws_iam_policy_document" "topic" {
-#   statement {
-#     effect = "Allow"
+data "aws_iam_policy_document" "topic" {
+  statement {
+    effect = "Allow"
 
-#     principals {
-#       type        = "Service"
-#       identifiers = ["s3.amazonaws.com"]
-#     }
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
 
-#     actions   = ["SNS:Publish"]
-#     resources = ["arn:aws:sns:*:*:s3-event-notification-topic"]
+    actions   = ["SNS:Publish"]
+    resources = ["arn:aws:sns:*:*:s3-event-notification-topic"]
 
-#     condition {
-#       test     = "ArnLike"
-#       variable = "aws:SourceArn"
-#       values   = [aws_s3_bucket.bucket.arn]
-#     }
-#   }
-# }
-# resource "aws_sns_topic" "topic" {
-#   name   = "s3-event-notification-topic"
-#   policy = data.aws_iam_policy_document.topic.json
-# }
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.bucket.arn]
+    }
+  }
+}
+resource "aws_sns_topic" "topic" {
+  name   = "s3-event-notification-topic"
+  policy = data.aws_iam_policy_document.topic.json
+}
 
-# resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
-#   topic_arn = aws_sns_topic.topic.arn
-#   protocol  = "email"
-#   endpoint  = "devopsdojo@yahoo.com"
-# }
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.topic.arn
+  protocol  = "email"
+  endpoint  = "devopsdojo@yahoo.com"
+}
 
-# resource "aws_s3_bucket" "bucket" {
-#   bucket = "some-name-1234567890"
-# }
+resource "aws_s3_bucket" "bucket" {
+  bucket = "some-name-1234567890"
+}
 
-# resource "aws_s3_bucket_notification" "bucket_notification" {
-#   bucket = aws_s3_bucket.bucket.id
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket.id
 
-#   topic {
-#     topic_arn     = aws_sns_topic.topic.arn
-#     events        = ["s3:ObjectCreated:*"]
-#   }
-# }
+  topic {
+    topic_arn     = aws_sns_topic.topic.arn
+    events        = ["s3:ObjectCreated:*"]
+  }
+}
